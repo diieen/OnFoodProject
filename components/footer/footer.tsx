018ea03f4aba1d6e14c2ae6fb128d527bf2a088e
Jean Ferreira <jean@prumo.digital>
2022-04-13 14:04:16 -0300

    added components header and footer

---
+++ components/footer/footer.tsx
@@ -0,0 +1,7 @@
+export function Footer() {
+    return (
+        <footer>
+            <p>OnFood Footer</p>
+        </footer>
+    )
+}
---
+++ components/header/header.tsx
@@ -0,0 +1,7 @@
+export function Header() {
+    return (
+        <header>
+            <h1>Header OnFood</h1>
+        </header>
+    )
+}
