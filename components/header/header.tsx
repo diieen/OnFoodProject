f78ec14a27721682722b774bc04d1c236326baed
Jean Ferreira <jean@prumo.digital>
2022-04-29 14:44:34 -0300

    preview

--- components/header/header.tsx
+++ components/header/header.tsx
@@ -1,7 +1,37 @@
+import Image from 'next/image';
+import Link from 'next/link';
+import Styles from './header.module.css';
+
 export function Header() {
     return (
         <header>
-            <h1>Header OnFood</h1>
+            <nav className={Styles.nav}>
+                <div className={Styles.brand}>
+                    <Link href="/">
+                        <a>
+                            <Image
+                                src="/images/logo.png"
+                                width={45}
+                                height={36}
+                                alt="OnFood"
+                            />
+                            <span>OnFood</span>
+                        </a>
+                    </Link>
+                </div>
+                <ul className="flex">
+                    <li>
+                        <Link href="/">
+                            <a>Home</a>
+                        </Link>
+                    </li>
+                    <li>
+                        <Link href="/favoritos">
+                            <a>Favoritos</a>
+                        </Link>
+                    </li>
+                </ul>
+            </nav>
         </header>
     )
 }
